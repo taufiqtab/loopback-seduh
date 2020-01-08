9e226fe37b5cb73049b031e309c9518e3492fe08
taufiqtab <taufiqtab77@gmail.com>
2020-01-08 11:14:26 +0700

    init project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {LoopbackSeduhApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {LoopbackSeduhApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new LoopbackSeduhApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
