2f05665f2354104ed51aa91529ba01cfbc89d3c7
taufiqtab <taufiqtab77@gmail.com>
2020-01-09 10:25:59 +0700

    accessModel

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -1,16 +1,26 @@
 // Uncomment these imports to begin using these cool features!
 
 // import {inject} from '@loopback/context';
-import { get, param } from '@loopback/rest';
+import { get, param, getFilterSchemaFor } from '@loopback/rest';
+import { User } from '../models';
+import { UserRepository } from '../repositories';
+import { repository, Filter } from '@loopback/repository';
 
 export class HelloController {
-  @get('/hello/{nama}/{vala}/{valb}')
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+  ) { }
+
+  @get('/hello/{nama}/{vala}/{valb}/{id}')
   async hello(
     @param.path.string('nama') nama: string,
     @param.path.number('vala') vala: number,
     @param.path.number('valb') valb: number,
+    @param.path.number('id') id: number,
   ): Promise<string> {
+    var user = this.userRepository.findById(id);
     var value = vala + valb;
-    return "hello " + nama + " hasil penjumlahan =  " + value;
+    return "hello " + nama + " hasil penjumlahan =  " + value + " " + (await user).nama;
   }
 }
